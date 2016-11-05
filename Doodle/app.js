fc6b63357d5a993d7893b1bde495292a060ea9c7
BuildTools <unconfigured@null.spigotmc.org>
2016-11-04 20:32:29 -0400

    Handlebars

--- Doodle/app.js
+++ Doodle/app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.render('main');
+  res.render('index');
 });
 
 // catch 404 and forward to error handler
