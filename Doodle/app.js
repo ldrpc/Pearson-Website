f0d0655f80cbde81ebdca3c7dd1b3e57f804cbea
BuildTools <unconfigured@null.spigotmc.org>
2016-11-04 20:16:07 -0400

    Handlebars

--- Doodle/app.js
+++ Doodle/app.js
@@ -29,11 +29,7 @@ app.get('/', function(req, res) {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+
 
 // error handler
 app.use(function(err, req, res, next) {
